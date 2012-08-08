9342d64f3c614a32ecafd2068d0de666f8ba7b73
Derek Springer <derekspringer@gmail.com>
2012-08-07 18:43:16 -0700

    Swapped out db storage for cached storage

--- uninstall.php
+++ uninstall.php
@@ -5,15 +5,5 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
 
-$recipes = get_option( 'beerxml_shortcode_recipes' );
-if ( $recipes ) {
-	foreach ( $recipes as $recipe )  {
-		delete_option( "beerxml_shortcode_recipe-$recipe" );
-	}
-}
-
-// Delete option from the options table
-delete_option( 'beerxml_shortcode_recipes' );
-
 // Remove and additional options and custom tables
 // TODO
