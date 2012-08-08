77acc5d30f2e7bf06b754eccc34053de52e92481
Derek Springer <derekspringer@gmail.com>
2012-08-07 18:44:01 -0700

    First working cut of fermentables, hops, and yeast
    Also, swapped out db recipe storage for cached storage

--- beerxml-shortcode.php
+++ beerxml-shortcode.php
@@ -14,17 +14,7 @@ class BeerXMLShortcode {
 	 * A simple call to init when constructed
 	 */
 	function __construct() {
-		register_activation_hook( __FILE__, array( $this, 'install' ) );
 		add_action( 'init', array( $this, 'init' ) );
-	}
-
-	/**
-	 * Do all the plugin activation setup
-	 */
-	function install() {
-		if( ! get_option( 'beerxml_shortcode_recipes' ) ) {
-			add_option( 'beerxml_shortcode_recipes', array(), '', 'no' );
-		}
 	}
 
 	/**
@@ -67,7 +57,9 @@ class BeerXMLShortcode {
 		}
 
 		extract( shortcode_atts( array(
-			'recipe'  => null
+			'recipe' => null,
+			'cache'  => 60*60*12,
+			'metric' => false
 		), $atts ) );
 
 		if ( ! isset( $recipe ) ) {
@@ -76,15 +68,20 @@ class BeerXMLShortcode {
 
 		$recipe = esc_url_raw( $recipe );
 		$recipe_filename = pathinfo( $recipe, PATHINFO_FILENAME );
-		$recipe_id = "{$post->ID}_$recipe_filename";
-		$beer_xml = get_option( "beerxml_shortcode_recipe-$recipe_id" );
-		if ( ! $beer_xml ) {
+		$recipe_id = "beerxml_shortcode_recipe-{$post->ID}_$recipe_filename";
+
+		$cache  = intval( esc_attr( $cache ) );
+		if ( -1 == $cache ) {
+			delete_transient( $recipe_id );
+			$cache = 0;
+		}
+
+		$metric = (boolean) esc_attr( $metric );
+
+		if ( ! $cache || false === ( $beer_xml = get_transient( $recipe_id ) ) ) {
 			$beer_xml = new BeerXML( $recipe );
-			if ( $beer_xml->recipes ) {
-				$recipes = get_option( 'beerxml_shortcode_recipes', array() );
-				$recipes[] = $recipe_id;
-				update_option( 'beerxml_shortcode_recipes', $recipes );
-				add_option( "beerxml_shortcode_recipe-$recipe_id", $beer_xml );
+			if ( $cache && $beer_xml->recipes ) {
+				set_transient( $recipe_id, $beer_xml, $cache );
 			}
 		}
 
@@ -92,7 +89,164 @@ class BeerXMLShortcode {
 			return '<!-- Error parsing BeerXML document -->';
 		}
 
-		return "<h1>{$beer_xml->recipes[0]->name}</h1>";
+		$fermentables = '';
+		foreach ( $beer_xml->recipes[0]->fermentables as $fermentable ) {
+			$fermentables .= $this->build_fermentable( $fermentable, $metric );
+		}
+
+		$t_fermentables = __( 'Fermentables', 'beerxml-shortcode' );
+		$t_name = __( 'Name', 'beerxml-shortcode' );
+		$t_amount = __( 'Amount', 'beerxml-shortcode' );
+		$fermentables = <<<FERMENTABLES
+		<div id='fermentables'>
+			<h2>$t_fermentables</h2>
+			<table>
+				<thead>
+					<tr>
+						<th>$t_name</th>
+						<th>$t_amount</th>
+					</tr>
+					$fermentables
+				</thead>
+			</table>
+		</div>
+FERMENTABLES;
+
+		$hops = '';
+		foreach ( $beer_xml->recipes[0]->hops as $hop ) {
+			$hops .= $this->build_hop( $hop, $metric );
+		}
+
+		$t_hops  = __( 'Hops', 'beerxml-shortcode' );
+		$t_time  = __( 'Time', 'beerxml-shortcode' );
+		$t_use   = __( 'Use', 'beerxml-shortcode' );
+		$t_form  = __( 'Form', 'beerxml-shortcode' );
+		$t_alpha = __( 'Alpha %', 'beerxml-shortcode' );
+		$hops = <<<HOPS
+		<div id='hops'>
+			<h2>$t_hops</h2>
+			<table>
+				<thead>
+					<tr>
+						<th>$t_name</th>
+						<th>$t_amount</th>
+						<th>$t_time</th>
+						<th>$t_use</th>
+						<th>$t_form</th>
+						<th>$t_alpha</th>
+					</tr>
+					$hops
+				</thead>
+			</table>
+		</div>
+HOPS;
+
+		$yeasts = '';
+		foreach ( $beer_xml->recipes[0]->yeasts as $yeast ) {
+			$yeasts .= $this->build_yeast( $yeast, $metric );
+		}
+
+		$t_yeast       = __( 'Yeast', 'beerxml-shortcode' );
+		$t_lab         = __( 'Lab', 'beerxml-shortcode' );
+		$t_attenuation = __( 'Attenuation', 'beerxml-shortcode' );
+		$t_temperature = __( 'Temperature', 'beerxml-shortcode' );
+		$yeasts = <<<YEASTS
+		<div id='yeasts'>
+			<h2>$t_yeast</h2>
+			<table>
+				<thead>
+					<tr>
+						<th>$t_name</th>
+						<th>$t_lab</th>
+						<th>$t_attenuation</th>
+						<th>$t_temperature</th>
+					</tr>
+					$yeasts
+				</thead>
+			</table>
+		</div>
+YEASTS;
+
+		$html = <<<HTML
+		<div id='beerxml-recipe'>
+			$fermentables
+			$hops
+			$yeasts
+		</div>
+HTML;
+
+		return $html;
+	}
+
+	function build_fermentable( $fermentable, $metric = false ) {
+		if ( $metric ) {
+			$fermentable->amount = round( $fermentable->amount, 3 );
+			$t_weight = __( 'kg', 'beerxml-shortcode' );
+		} else {
+			$fermentable->amount = round( $fermentable->amount * 2.20462, 2 );
+			$t_weight = __( 'lbs', 'beerxml-shortcode' );
+		}
+
+		return <<<FERMENTABLE
+		<tr>
+			<td>$fermentable->name</td>
+			<td>$fermentable->amount $t_weight</td>
+		</tr>
+FERMENTABLE;
+	}
+
+	function build_hop( $hop, $metric = false ) {
+		if ( $metric ) {
+			$hop->amount = round( $hop->amount * 1000, 1 );
+			$t_weight = __( 'g', 'beerxml-shortcode' );
+		} else {
+			$hop->amount = round( $hop->amount * 35.274, 2 );
+			$t_weight = __( 'oz', 'beerxml-shortcode' );
+		}
+
+		if ( $hop->time >= 1440 ) {
+			$hop->time = round( $hop->time / 1440, 1);
+			$t_time = _n( 'day', 'days', $hop->time, 'beerxml-shortcode' );
+		} else {
+			$hop->time = round( $hop->time );
+			$t_time = __( 'min', 'beerxml-shortcode' );
+		}
+
+		$hop->alpha = round( $hop->alpha, 1 );
+
+		return <<<FERMENTABLE
+		<tr>
+			<td>$hop->name</td>
+			<td>$hop->amount $t_weight</td>
+			<td>$hop->time $t_time</td>
+			<td>$hop->use</td>
+			<td>$hop->form</td>
+			<td>$hop->alpha</td>
+		</tr>
+FERMENTABLE;
+	}
+
+	function build_yeast( $yeast, $metric = false ) {
+		if ( $metric ) {
+			$yeast->min_temperature = round( $yeast->min_temperature, 2 );
+			$yeast->max_temperature = round( $yeast->max_temperature, 2 );
+			$t_temp = __( 'C', 'beerxml-shortcode' );
+		} else {
+			$yeast->min_temperature = round( ( $yeast->min_temperature * (9/5) ) + 32, 1 );
+			$yeast->max_temperature = round( ( $yeast->max_temperature * (9/5) ) + 32, 1 );
+			$t_temp = __( 'F', 'beerxml-shortcode' );
+		}
+
+		$yeast->attenuation = round( $yeast->attenuation );
+
+		return <<<YEAST
+		<tr>
+			<td>$yeast->name</td>
+			<td>$yeast->laboratory</td>
+			<td>{$yeast->attenuation}%</td>
+			<td>{$yeast->min_temperature}°$t_temp - {$yeast->max_temperature}°$t_temp</td>
+		</tr>
+YEAST;
 	}
 }
 
