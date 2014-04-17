eb82303e7bda973bb0777fd5f1f426ee919a802b
Derek Springer <derekspringer@gmail.com>
2014-04-16 19:04:00 -0700

    Remove options (including multisite) upon uninstall.

--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,27 @@
 <?php
 
-// If uninstall not called from WordPress exit
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+if ( ! defined( 'ABSPATH' ) || ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
 
-// Remove and additional options and custom tables
-// TODO
+if ( ! is_multisite() ) {
+	delete_option( 'beerxml_shortcode_cache' );
+	delete_option( 'beerxml_shortcode_units' );
+	delete_option( 'beerxml_shortcode_download' );
+	delete_option( 'beerxml_shortcode_style' );
+} else {
+	global $wpdb;
+	$blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
+	$original_blog_id = get_current_blog_id();
+	foreach ( $blog_ids as $blog_id ) {
+		switch_to_blog( $blog_id );
+		delete_option( 'beerxml_shortcode_cache' );
+		delete_option( 'beerxml_shortcode_units' );
+		delete_option( 'beerxml_shortcode_download' );
+		delete_option( 'beerxml_shortcode_style' );
+	}
+
+	switch_to_blog( $original_blog_id );
+}
+
+// drops mic
